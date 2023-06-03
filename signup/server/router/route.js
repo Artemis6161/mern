31be72cfde2f8ac94a97e92f99ba3a80db21a3fc
sangeetha <Artemis6161@github.com>
2023-06-04 00:07:46 +0530

    route

--- signup/server/router/route.js
+++ signup/server/router/route.js
@@ -1,20 +1,16 @@
 import { Router } from "express";
 const router = Router();
 
-import * as controller from '../controllers/appController';
-
-// post Method
-router.route('/register').post(controller.register);
-// router.route('/registerMail').post( );
-router.route('/authentication').post((req,res)=> res.end());
-router.route('/login').post(controller.login);
-
-
-// Get Method
-router.route('/user/:username').get(controller.getUser );
-
-// put Method
-router.route('/updateuser').put(controller.updateUser);
+// post method
+router.route('/register').post((req,res)=> res.json('register route'));
+router.route('/registerMail').post()
+router.route('/authentication').post()
+router.route('/login').post()
+// get method 
+router.route('/user/:username').get()
 
 
-export default router; 
+// put method
+router.route('/updateuser').put()
+
+export default router;
